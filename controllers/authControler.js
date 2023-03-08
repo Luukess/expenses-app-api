e481089b24243a9147c3a4e2638b75c960e744ea
Lukasz Chwiedosik <lukasz.chwiedosik@gmail.com>
2023-03-08 21:13:24 +0100

    Feat: Added email verification before creating an account

--- controllers/authControler.js
+++ controllers/authControler.js
@@ -10,11 +10,16 @@ const handleRegisterUser = async (req, res) => {
     const { userName, email, password, confirmRegulation } = req.body;
     try {
         const validation = await handleRegisterValidation(userName, email, password, confirmRegulation);
+        const isUserExist = await User.findOne({email: email});
 
-        if (!validation.error) {
-            const hashedPassword = await bcrypt.hash(password, 10);
-            const handleAddUser = await User.create({ userName: userName, email: email, password: hashedPassword, confirmRegulation: confirmRegulation });
-            res.status(201).send({ message: 'Registration completed successfully' });
+        if(isUserExist){
+           res.status(409).send({message: 'There is already an account with this email'});
+        }else{
+            if (!validation.error) {
+                const hashedPassword = await bcrypt.hash(password, 10);
+                const handleAddUser = await User.create({ userName: userName, email: email, password: hashedPassword, confirmRegulation: confirmRegulation });
+                res.status(201).send({ message: 'Registration completed successfully' });
+            };
         };
 
     } catch (e) {
@@ -22,7 +27,7 @@ const handleRegisterUser = async (req, res) => {
         if (error || e.errors) {
             res.status(400).send({ message });
         } else {
-            res.status(500).send({ mesage: 'Problem connecting to the server' });
+            res.status(500).send({ message: 'Problem connecting to the server' });
         };
     };
 };
